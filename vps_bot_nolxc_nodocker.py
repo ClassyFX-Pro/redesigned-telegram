3837db46f2455646045cf1857783fa76a3016e9f
ClassyFX-Pro <jandelzm@gmail.com>
2026-07-26 20:29:12 +0530

    Update vps_bot_nolxc_nodocker.py

--- vps_bot_nolxc_nodocker.py
+++ vps_bot_nolxc_nodocker.py
@@ -623,9 +623,9 @@ def truncate_text(text, max_length=1024):
 def create_embed(title, description="", color=0x1a1a1a):
     embed = discord.Embed(title=truncate_text(f"🌟 {BOT_NAME} - {title}", 256),
                            description=truncate_text(description, 4096), color=color)
-    embed.set_thumbnail(url="https://i.imgur.com/Tv3clt0.jpeg")
+    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1530821352032964682/1530828818472898590/b6a2ddc9ad5a879f723f5cc9f238471d.png?ex=6a66feef&is=6a65ad6f&hm=e7e16b8c57980eab1b4a6ed9c8088cfa764e27ca8ca66e28964fe081b4ca67db")
     embed.set_footer(text=f"{BOT_NAME} VPS Manager v{BOT_VERSION} • {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}",
-                      icon_url="https://i.imgur.com/Tv3clt0.jpeg")
+                      icon_url="https://cdn.discordapp.com/attachments/1530821352032964682/1530828818472898590/b6a2ddc9ad5a879f723f5cc9f238471d.png?ex=6a66feef&is=6a65ad6f&hm=e7e16b8c57980eab1b4a6ed9c8088cfa764e27ca8ca66e28964fe081b4ca67db")
     return embed
 
 def add_field(embed, name, value, inline=False):
